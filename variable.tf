d6b764cc44698600015bbc3c6c74650dea511487
vishnuedathodi <154342576+vishnuedathodi@users.noreply.github.com>
2024-03-18 11:55:29 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,5 +1,10 @@
 variable "cluster_name" {
   description = "cluster name"
   type        = string
-  default     = ""
+  default     = " "
+}
+variable "release_name" {
+  description = "Helm release name"
+  type        = string
+  default     = " "
 }
